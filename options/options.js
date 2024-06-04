bd5749445becb7bfa082dc02004c338828d72549
Crowdbotics <team@crowdbotics.com>
2024-06-04 23:25:48 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "viola-2-48264",
+  url: "https://viola-2-48264.botics.co",
+  api: "https://viola-2-48264.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
